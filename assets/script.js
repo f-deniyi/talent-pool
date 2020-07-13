69114dd90b4240ad766c16a570458bc80a5d7816
femi <61956087+f-deniyi@users.noreply.github.com>
2020-07-13 16:24:11 +0100

    assets folder

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+let slides = document.querySelectorAll(".slide");
+let counter = 0;
+function showAboutMe(count) {
+  for (var i = 0; i < slides.length; j++) {
+    slides[i].style.display = "none";
+  }
+}
+showAboutMe(counter);
